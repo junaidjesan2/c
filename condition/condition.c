be2c80ada12abbea76c5c3d76202612789921360
junaidjesan2 <junaidjesan2@gmail.com>
2024-04-01 02:17:16 +0600

    nested condition created

--- condition/condition.c
+++ condition/condition.c
@@ -1,21 +1,43 @@
 #include<stdio.h>
 
 int main(){
-     int tiffin;
+
+
+//    if(tiffin<=50)
+//    {
+//     printf("singara somucha khawa lagbo");
+//    }
+//    else if( tiffin <= 100)
+//    {
+//     printf("ebar sathe akta coke o khawa jaibo");
+//    } 
+//    else if(tiffin >100)
+//    {
+//     printf("nah akta burger e khabo");
+//    }
+//    else {printf("tui gorid tor kase taka ni");}
+
+int tiffin;
    scanf("%d",&tiffin);
-   if(tiffin<=50)
-   {
-    printf("singara somucha khawa lagbo");
-   }
-   else if( tiffin <= 100)
-   {
-    printf("ebar sathe akta coke o khawa jaibo");
-   } 
-   else if(tiffin >100)
-   {
-    printf("nah akta burger e khabo");
-   }
-   else {printf("tui gorid tor kase taka ni");}
+
+   int x = 5;
+        // if (x < 10) {
+        //     printf("hello ");
+        // }
+        // if (x == 5) {
+        //     printf("hi");
+        // }
+        // else {
+        //     printf("no");
+        // }
+
+        int x=0;
+        if (x=1) {
+            printf("hi");
+        }
+        else {
+            printf("how are u");
+        }
    
     return 0;
 }
