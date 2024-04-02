d0d54c7a3ecb3e6f000e3aae3ed28fd69c900f77
junaidjesan2 <junaidjesan2@gmail.com>
2024-04-02 22:55:45 +0600

    explored break rules using for loop

--- week-01/M-3-loop/conditional-loop/loop.c
+++ week-01/M-3-loop/conditional-loop/loop.c
@@ -3,15 +3,34 @@
 
 int main()
 {
+    // int sum=0;
+
+    // for (int i = 0; i <= 10; i++)
+    // {
+    //     int divi=i%2;
+
+    // if (divi==0)
+    // {
+    //     sum += i;
+    // }
+    // }
+    // printf("%d",sum);
+
+
     int sum=0;
 
-    for (int i = 0; i <= 10; i++)
+    for (int i = 0; i <= 100; i++)
     {
         int divi=i%2;
-        
+
     if (divi==0)
     {
         sum += i;
+        if (sum >500)
+        {
+            break;
+        }
+        
     }
     }
     printf("%d",sum);
