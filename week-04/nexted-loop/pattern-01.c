ccbd947ee813fe4a187980511408968ecaac4a96
junaidjesan2 <junaidjesan2@gmail.com>
2024-04-29 11:29:55 +0600

    nested loop partern one commited

---
+++ week-04/nexted-loop/pattern-01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{  
+    int N, K=1;
+    scanf("%d",&N);
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= K; j++)
+        {
+            printf("*");
+        }
+        K++;
+        printf("\n");
+    }
+    
+    return 0;
+}
