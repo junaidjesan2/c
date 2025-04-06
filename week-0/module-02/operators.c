98e297607aa4868a84a96b494fe1999cca5ef4a7
junaidjesan2 <junaidjesan2@gmail.com>
2025-04-07 01:44:30 +0600

    started practicing c and it has begin with operators

---
+++ week-0/module-02/operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int sum = a + 5; // Addition operator
+    int diff = a - sum; // Subtraction operator
+    int prod = a * diff; // Multiplication operator
+    float div = (float)a / prod; // Division operator
+
+    scanf("%d", &a); // Input operator
+    printf("Sum: %d\n", sum); // Output operator
+
+    return 0;
+}
