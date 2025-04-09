19df63272140b46f053798387dfb182560f957a8
junaidjesan2 <junaidjesan2@gmail.com>
2025-04-10 01:48:55 +0600

    problem 4 solved

---
+++ week-0/module-3.5/problem4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+
+    if (1 <= N <= 1000)
+    {
+        /* code */
+        for (int i = 0; i < N; i++)
+        {
+            printf("I Love Practice\n");
+        }
+    }
+
+    return 0;
+}
