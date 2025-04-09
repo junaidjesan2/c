c09a90a4639f40cd9a6f7b04a27d8e8991f32a06
junaidjesan2 <junaidjesan2@gmail.com>
2025-04-10 01:30:40 +0600

    first problem of modul 3.5 has done

--- week-0/module-02/evenOrOdd.c
+++ week-0/module-02/evenOrOdd.c
@@ -1,11 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
     int a;
     int b;
     scanf("%d", &a);
     scanf("%d", &b);
-    if(a % 2 == 0 && b % 2 == 0){
-        printf("Both are even\n");
+    if (a > 0)
+    {
+        printf("A is positive number\n");
+    }
+    else
+    {
+        printf("A is negative number\n");
+    }
+    if (b > 0)
+    {
+        printf("B is positive number\n");
+    }
+    else
+    {
+        printf("B is negative number\n");
     }
 }
