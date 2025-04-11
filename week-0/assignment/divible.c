18e3fc50bb0b448d697f2c492e4a7223dd4929a2
junaidjesan2 <junaidjesan2@gmail.com>
2025-04-11 14:39:22 +0600

    solved problem three

---
+++ week-0/assignment/divible.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    long long int N;
+    scanf("%lld",&N);
+
+    if (N%3==0)
+    {
+        printf("YES\n");
+    }
+    else{
+        printf("NO\n");
+    }
+    return 0;
+}
