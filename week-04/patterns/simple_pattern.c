cbd785c0d0c38ca12afa3d443cf99e71d5bd1b02
junaidjesan2 <junaidjesan2@gmail.com>
2025-05-03 21:54:24 +0600

    pyramid pattern solved

--- week-04/patterns/simple_pattern.c
+++ week-04/patterns/simple_pattern.c
@@ -4,13 +4,20 @@ int main()
 {
     int n;
     scanf("%d", &n);
+    int star = 1;
     for (int i = 1; i <= n; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int k = n - i; k >= 1; k--)
+        {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= star; j++)
         {
             printf("*");
         }
         printf("\n");
+        star += 2;
     }
 
     return 0;
